ec909783c6f9de4d0b1d9f057d1cadac924d7148
Kelvys <contato@kelvysb.com>
2020-02-19 23:02:46 -0300

    Adjuts

--- API/PublishWin64.ps1
+++ API/PublishWin64.ps1
@@ -3,4 +3,5 @@ if (-not (Test-Path -Path .\MagicCucaBakeryServer -PathType Container))
     mkdir .\MagicCucaBakeryServer
 }
 dotnet publish .\MagicCucaBakeryAPI\MagicCucaBakery.API.csproj -r win-x64 -c release --output .\MagicCucaBakeryServer
-Pause
+Set-Location .\MagicCucaBakeryServer
+Start-Process "MagicCucaBakery.API.exe"
